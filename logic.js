5556ea35cc7cb4cedd899a956391dd675ad65d85
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-04-20 22:43:33 +0300

    addition logic and logic test of remove function

--- logic.js
+++ logic.js
@@ -1,34 +1,20 @@
-// logic.js
-
-
-
+// LOGICS USED
 // 1. TOGGLE MENU STATE LOGIC
-
 function toggleMenuState(isActive){
   return {
     isActive: !isActive,
     text: !isActive ? "X" : "☰"
   };
 }
-
-
-
 // 2. FILTER PRODUCTS (SEARCH)
-
 function filterProducts(products, searchText){
   return products.filter(product =>
     product.title.toLowerCase().includes(searchText.toLowerCase().trim())
   );
 }
-
-
-
 // 3. ADD TO CART LOGIC
-
 function addToCartLogic(cart, product){
-
   const existingItem = cart.find(item => item.id == product.id);
-
   if (existingItem){
     existingItem.quantity += 1;
   } else {
@@ -40,23 +26,22 @@ function addToCartLogic(cart, product){
       quantity: 1
     });
   }
-
   return cart;
 }
-
-
-
 // 4. CART TOTAL COUNT
-
 function updateCartCountLogic(cart){
   return cart.reduce((total, item) => total + item.quantity, 0);
 }
-
-
+// 5. REMOVE FUNTION IN CART
+function removeItem(cart, index){
+  cart.spice(index, 1);
+return cart;
+}
 // EXPORT FUNCTIONS
 module.exports = {
   toggleMenuState,
   filterProducts,
   addToCartLogic,
-  updateCartCountLogic
+  updateCartCountLogic,
+  removeItem
 };
--- logic.test.js
+++ logic.test.js
@@ -2,110 +2,92 @@ const {
   toggleMenuState,
   filterProducts,
   addToCartLogic,
-  updateCartCountLogic
+  updateCartCountLogic,
+  
+  removeItem
 } = require('./logic');
-
-
-
-// TEST 1: MENU TOGGLE
-
-        test('toggles menu from closed to open', () => {
+// 1: MENU TOGGLE
+        test("toggles menu from closed to open", () => {
   const result = toggleMenuState(false);
-
     expect(result.isActive).toBe(true);
   expect(result.text).toBe("X");
 });
-
-        test('toggles menu from open to closed', () => {
+        test("toggles menu from open to closed", () => {
   const result = toggleMenuState(true);
-
     expect(result.isActive).toBe(false);
   expect(result.text).toBe("☰");
 });
-
-
-
-// TEST 2: FILTER PRODUCTS
-
-        test('filters products using search text', () => {
+// 2: FILTER PRODUCTS
+        test("filters products using search text", () => {
   const products = [
     { title: "iPhone" },
     { title: "Laptop" },
     { title: "Headphones" }
   ];
-
     const result = filterProducts(products, "phone");
-
   expect(result.length).toBe(1);
   expect(result[0].title).toBe("iPhone");
 });
-
-test('returns empty array if no match', () => {
+test("returns empty array if no match", () => {
   const products = [
     { title: "Laptop" },
-    { title: "Tablet" }
+    { title: "Laptop" }
   ];
-
   const result = filterProducts(products, "phone");
-
   expect(result.length).toBe(0);
 });
-
-
-
-// TEST 3: ADD TO CART
-test('adds new product to cart', () => {
+// 3: ADD TO CART
+test("adds new product to cart", () => {
   let cart = [];
-
   const product = {
     id: 1,
     title: "Phone",
     price: 100,
     image: "img.jpg"
   };
-
   cart = addToCartLogic(cart, product);
-
   expect(cart.length).toBe(1);
   expect(cart[0].quantity).toBe(1);
 });
-
-test('increases quantity if product exists', () => {
+test("increases quantity if product exists", () => {
   let cart = [
     { id: 1, title: "Phone", price: 100, quantity: 1 }
   ];
-
   const product = {
     id: 1,
     title: "Phone",
     price: 100,
     image: "img.jpg"
   };
-
   cart = addToCartLogic(cart, product);
-
   expect(cart.length).toBe(1);
   expect(cart[0].quantity).toBe(2);
 });
-
-
-
-// TEST 4: CART COUNT
-
-test('calculates total cart quantity correctly', () => {
+// 4: CART COUNT
+test("calculates total cart quantity correctly", () => {
   const cart = [
     { quantity: 2 },
     { quantity: 3 },
     { quantity: 1 }
   ];
-
   const result = updateCartCountLogic(cart);
-
   expect(result).toBe(6);
 });
-
 test('returns 0 for empty cart', () => {
   const result = updateCartCountLogic([]);
-
   expect(result).toBe(0);
 });
+//5.REMOVE ITEMS
+test("remove item from cart by index",() =>{
+  let cart = [
+    {id:1, title: "watch"},
+    {id:2, title: "phone"},
+    {id:3, title: "laptop"}
+  ];
+  const result = removeItem(cart, 1);
+  expect(result).toEqual([{id:1, title: "watch"},
+    {id:3, title: "laptop"}
+  ])
+}
+
+);
